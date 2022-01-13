4edf7dd1d2f1d2afd3bb43b5312c8ff15c3633a2
kevorkkeheian <kevorkkeheian@gmail.com>
2022-01-13 21:53:43 +0200

    class: change the default country in company (temp)

--- Shared/Models/Organization/Company.cs
+++ Shared/Models/Organization/Company.cs
@@ -15,7 +15,7 @@ namespace Commerce.Shared.Models.Organization
         public string Slug { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
-        public int CountryId { get; set; } = 345;
+        public int CountryId { get; set; } = 1;
         public Country? Country { get; set; }
         public string? City { get; set; }
         public string? Address { get; set; }
